b3383c198bebacefb3f5ed2a2a11973d19af691b
Brian Charous <brian.charous@gmail.com>
2015-03-14 15:59:27 -0500

    cluster is goin'

--- base.py
+++ base.py
@@ -1,6 +1,7 @@
 from collections import namedtuple
 from enum import Enum
 import json
+import numpy as np
 import os
 import xml.etree.ElementTree as ET
 
@@ -9,13 +10,72 @@ class Chamber(Enum):
     house = 1
     senate = 2
 
+class VoteRecord(object):
+    """ abstraction of voting record backed up by numpy matrix """
+
+    def _init_structure(self):
+        """ initialize the matrix data sturcture """
+        # matrix like
+        #            | bill_id 1 yay | bill_id 1 nay | bill_id 1 not voting | bill_id 1 present | ...
+        # voter_id 1 |       1       |       0       |          0           |         0         |
+        # voter_id 2 |       0               1       |          0           |         0         |
+        # .......... | 
+        
+        # create dictionary to map voter id to row in the matrix
+        # {voter_id: row #}
+        for i in range(len(self.voter_ids_list)):
+            self.voter_row_mappings[self.voter_ids_list[i]] = i
+            
+        # dictionary to map bill id to (start) column in the matrix
+        # {bill_id: start column #}
+        bill_ids = list(self.record.keys())
+        for i in range(len(bill_ids)):
+            self.bill_col_mappings[bill_ids[i]] = i*4
+
+        self.matrix = np.zeros((len(self.voter_ids), 4*len(self.record)))
+        for bill_id in self.record:
+            vote = self.record[bill_id]
+            yays = vote.yays
+            nays = vote.nays
+            not_voting = vote.not_voting
+            present = vote.present
+            for vid in yays:
+                row = self.voter_row_mappings[vid]
+                column = self.bill_col_mappings[bill_id]
+                self.matrix[row][column] = 1
+            for vid in nays:
+                row = self.voter_row_mappings[vid]
+                column = self.bill_col_mappings[bill_id]+1
+                self.matrix[row][column] = 1
+            for vid in not_voting:
+                row = self.voter_row_mappings[vid]
+                column = self.bill_col_mappings[bill_id]+2
+                self.matrix[row][column] = 1
+            for vid in present:
+                row = self.voter_row_mappings[vid]
+                column = self.bill_col_mappings[bill_id]+3
+                self.matrix[row][column] = 1
+
+    def __init__(self, record, voter_ids):
+        super(VoteRecord, self).__init__()
+        self.record = record
+        self.voter_ids = voter_ids
+        self.voter_ids_list = list(self.voter_ids)
+        self.matrix = None    
+        self.voter_row_mappings = {}
+        self.bill_col_mappings = {}
+
+        self._init_structure()
+
 
 class DataManager(object):
 
-    def voting_record(self, dir, chamber):
+    def parse_votes(self, dir, chamber):
+        """ read vote record from all data files in a given directory for house or senate """
         paths = self._collect_data_paths(dir, chamber)
         vote_record = {}
         Vote = namedtuple('Vote', ['yays', 'nays', 'not_voting', 'present'])
+        voters = set()
         for path in paths:
             with open(os.path.abspath(path), 'r') as f:
                 root = ET.fromstring(f.read())
@@ -27,6 +87,7 @@ class DataManager(object):
                 for child in root:
                     if child.tag == "voter":
                         voter_id = child.attrib['id']
+                        voters.add(voter_id)
                         vote = child.attrib['vote']
                         if vote == '+':
                             yays.append(voter_id)
@@ -41,7 +102,7 @@ class DataManager(object):
                             not_voting = not_voting,
                             present = present)
                 vote_record[vote_id] = vote
-        return vote_record
+        return vote_record, voters
 
     def _vote_id(self, attributes):
         prefix = ''
@@ -73,13 +134,14 @@ class DataManager(object):
                             os.path.join(os.path.abspath(path), file))
         return file_paths
 
-    def __init__(self,):
+    def __init__(self):
         super(DataManager, self).__init__()
 
 
 def main():
     d = DataManager()
-    print(d.voting_record('.', Chamber.house))
+    votes, ids = d.parse_votes('.', Chamber.house)
+    r = VoteRecord(votes, ids)
 
 if __name__ == '__main__':
     main()
